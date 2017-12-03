b0696b7fa5e73423e9ec9a33e5324751c2884a53
Eric Johnson <0216johnsoneric@gmail.com>
2017-12-03 19:55:44 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,9 +1,9 @@
 def hello_t(names)
   names.each{ |name| puts name }
-    puts "We're in the method!"
-    puts "Yielding to the block..."
-  yield
-    puts "We're back in the method!"
+  #   puts "We're in the method!"
+  #   puts "Yielding to the block..."
+  # yield
+  #   puts "We're back in the method!"
 end
 
 #
